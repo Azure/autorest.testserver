8ea4c5bf7b6d90ae96f5f400d50ff3b9dde81d7d
Johannes Bader <jogus@live.de>
2017-11-13 16:48:26 -0800

    bump

--- routes/report.js
+++ routes/report.js
@@ -1,20 +1,25 @@
-var express = require('express');
-var router = express.Router();
-var util = require('util');
+const express = require('express');
+const router = express.Router();
+const util = require('util');
+const { writeFileSync, unlinkSync } = require("fs");
 
-var report = function(coverage, azureCoverage, optionalCoverage) {
+const report = function(coverage, azureCoverage, optionalCoverage) {
 
-  const writeOutCoverageReport = () => require("fs").writeFileSync(
-    require("path").join(__dirname, "../report.json"), 
-    JSON.stringify({ vanilla: coverage, azure: azureCoverage, optional: optionalCoverage }, null, 2));
+  const writeOutCoverageReport = (path, object) => {
+    try {
+      require("fs").writeFileSync(
+        require("path").join(__dirname, path), 
+        JSON.stringify(object, null, 2));
+    } catch (_) { }
+  };
 
   router.get('/', function(req, res, next) {
-    writeOutCoverageReport();
+    writeOutCoverageReport("../report-vanilla.json", coverage);
     res.status(200).end(JSON.stringify(coverage));
   });
 
   router.get('/azure', function(req, res, next) {
-    writeOutCoverageReport();
+    writeOutCoverageReport("../report-azure.json", azureCoverage);
     res.status(200).end(JSON.stringify(azureCoverage));
   });
 }
