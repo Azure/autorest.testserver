2023ac714241413dcfd8c774e702cce9dbee9108
Amar Zavery <amzavery@microsoft.com>
2017-12-13 15:31:24 -0800

    add tests for additionalProperties (#34)
    * ensure enum value in the swagger matches to what the test server expects.
    
    * add tests for additionalProperties
    
    * adding a new model and an operation for that and removed type object from PetAPTrue
    
    * apInPropertiesWithAPString

--- app.js
+++ app.js
@@ -37,9 +37,9 @@ var parameterGrouping = require('./routes/azureParameterGrouping.js');
 var validation = require('./routes/validation.js');
 var customUri = require('./routes/customUri.js');
 var extensibleEnums = require('./routes/extensibleEnums.js');
+var additionalProperties = require('./routes/additionalProperties.js');
 var xml = require('./routes/xml.js'); // XML serialization
 var util = require('util');
-
 var app = express();
 
 //set up server log
@@ -65,6 +65,11 @@ var optionalCoverage = {
   "putDecimalBigPositiveDecimal" : 0
 };
 var coverage = {
+  "additionalPropertiesTrue": 0,
+  "additionalPropertiesTypeObject": 0,
+  "additionalPropertiesTypeString": 0,
+  "additionalPropertiesInProperties": 0,
+  "additionalPropertiesInPropertiesWithAPTypeString": 0,
   "getArrayNull": 0,
   "getArrayEmpty": 0,
   "putArrayEmpty": 0,
@@ -511,6 +516,7 @@ app.use('/parameterGrouping', new parameterGrouping(azurecoverage).router);
 app.use('/validation', new validation(coverage).router);
 app.use('/customUri', new customUri(coverage).router);
 app.use('/extensibleEnums', new extensibleEnums(coverage).router);
+app.use('/additionalProperties', new additionalProperties(coverage).router);
 app.use('/xml', new xml().router);
 
 // catch 404 and forward to error handler
