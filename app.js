23eb8f04399596f2efea1a8828f080946edfc049
Amar Zavery <amzavery@microsoft.com>
2018-06-20 13:58:46 -0700

    moving from jade to pug since jad has been deprecated. ran autorest.nodejs tests locally against the new dependency changes and everything worked fine. (#72)

--- app.js
+++ app.js
@@ -492,7 +492,7 @@ var coverage = {
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
