1d0eecbf5485f77ed9ab52bc37de4f2a4b577011
Pavel Krymets <pavel@krymets.com>
2019-11-20 14:59:58 -0800

    Clear endpoint

--- coverage/coverageEndpoint.js
+++ coverage/coverageEndpoint.js
@@ -5,6 +5,14 @@ var coverageEndpoint = function(coverage) {
     router.get('/', function(req, res, next) {
         res.status(200).send(coverage);
     });
+
+    router.post("/clear", function(req, res, next) {
+        for (var property in Object.getOwnPropertyNames(coverage))
+        {
+            coverage[property] = 0;
+        }
+        res.status(200).send();
+    });
 }
 
 coverageEndpoint.prototype.router = router;
