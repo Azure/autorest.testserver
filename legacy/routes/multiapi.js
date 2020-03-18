4be884f266fd6a51cc41ff854ce73c4db563d22b
iscai-msft <iscai@microsoft.com>
2020-03-18 17:32:42 -0400

    added optional coverage

--- legacy/routes/multiapi.js
+++ legacy/routes/multiapi.js
@@ -3,12 +3,22 @@ var router = express.Router();
 var utils = require('../util/utils');
 
 var multiapi = function(coverage) {
-    coverage['Multiapi'] = 0;
+    optionalCoverage['putTestOneApiVersionOne'] = 0;
+    optionalCoverage['putTestOneApiVersionTwo'] = 0;
+    optionalCoverage['getTestTwoApiVersionOne'] = 0;
+    optionalCoverage['getTestTwoApiVersionTwo'] = 0;
+    optionalCoverage['getTestTwoApiVersionThree'] = 0;
+    optionalCoverage['putTestThreeApiVersionTwo'] = 0;
+    optionalCoverage['putTestFourApiVersionTwo'] = 0;
+    optionalCoverage['putTestFourApiVersionThree'] = 0;
+    optionalCoverage['putTestFiveApiVersionThree'] = 0;
 
     router.put('/testOneEndpoint', function (req, res, next) {
         if (req.query["api-version"] === '1.0.0') {
+            optionalCoverage['putTestOneApiVersionOne']++;
             res.status(200).end();
         } else if (req.query["api-version"] === '2.0.0') {
+            optionalCoverage['putTestOneApiVersionTwo']++;
             res.status(200).type('json').end('{ "id": "1", "message": "This was called with api-version 2.0.0" }');
         } else {
             utils.send400(res, next, "The api version of the operation mixin is not supported: " + req.query['api-version']);
@@ -16,10 +26,13 @@ var multiapi = function(coverage) {
     });
     router.get('/one/testTwoEndpoint', function(req, res, next) {
         if (req.query["api-version"] == '1.0.0') {
+            optionalCoverage['getTestTwoApiVersionOne']++;
             res.status(200).end()
         } else if (req.query["api-version"] == '2.0.0') {
+            optionalCoverage['getTestTwoApiVersionTwo']++;
             res.status(200).type('json').end('{ "id": "1", "message": "This was called with api-version 2.0.0" }');
         } else if (req.query["api-version"] == '3.0.0') {
+            optionalCoverage['getTestTwoApiVersionThree']++;
             res.status(200).type('json').end('{ "optionalProperty": "This was called with api-version 3.0.0" }');
         } else {
             utils.send400(res, next, "The api version of testTwo is not supported: " + req.query['api-version']);
@@ -27,6 +40,7 @@ var multiapi = function(coverage) {
     });
     router.put('/one/testThreeEndpoint', function(req, res, next) {
         if (req.query["api-version"] == '2.0.0') {
+            optionalCoverage['putTestThreeApiVersionTwo']++;
             res.status(200).end();
         } else {
             utils.send400(res, next, "The api version of testThree is not supported: " + req.query['api-version']);
@@ -34,8 +48,10 @@ var multiapi = function(coverage) {
     });
     router.put('/two/testFourEndpoint', function(req, res, next) {
         if (req.query["api-version"] == '2.0.0') {
+            optionalCoverage['putTestFourApiVersionTwo']++;
             res.status(200).end();
         } else if (req.query["api-version"] == '3.0.0') {
+            optionalCoverage['putTestFourApiVersionThree']++;
             res.status(200).end();
         } else {
             utils.send400(res, next, "The api version of testFour is not supported: " + req.query['api-version']);
@@ -43,6 +59,7 @@ var multiapi = function(coverage) {
     });
     router.put('/two/testFiveEndpoint', function(req, res, next) {
         if (req.query["api-version"] == '3.0.0') {
+            optionalCoverage['putTestFiveApiVersionThree']++;
             res.status(200).end();
         } else {
             utils.send400(res, next, "The api version of testFive is not supported: " + req.query['api-version']);
