8b01de69c0d54fc4062ef55c6a65d7a2eed47fcb
iscai-msft <iscai@microsoft.com>
2020-04-06 11:54:26 -0400

    switched posted non string enum float

--- legacy/routes/nonStringEnums.js
+++ legacy/routes/nonStringEnums.js
@@ -24,7 +24,7 @@ var nonStringEnums = function(coverage) {
     });
     router.put('/float/put', function(req, res, next) {
         let body = req.body;
-        if (body == '200.0') {
+        if (body == '200.4') {
             coverage['NonStringEnumsPostFloat']++;
             res.status(200).json("Nice job posting a float enum");
         }
