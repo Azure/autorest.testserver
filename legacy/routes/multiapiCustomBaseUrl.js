30af4ebacbac8425a28735d1bc74afe87de9c392
iscai-msft <iscai@microsoft.com>
2020-07-16 18:04:12 -0400

    add test for multiapi custom base url with diff host templates

---
+++ legacy/routes/multiapiCustomBaseUrl.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var utils = require('../util/utils');
+
+var multiapiCustomBaseUrl = function(optionalCoverage) {
+    optionalCoverage['MultiapiCustomBaseUrlApiVersionOne'] = 0;
+    optionalCoverage['MultiapiCustomBaseUrlApiVersionTwo'] = 0;
+
+    router.put('/v1/test', function (req, res, next) {
+        if (req.query["api-version"] === '1.0.0' && req.query["id"] == 1) {
+            optionalCoverage['MultiapiCustomBaseUrlApiVersionOne']++;
+            res.status(200).end();
+        } else {
+            utils.send400(res, next, "Api version needs to be 1.0.0 and you should pass in 1 for 'id'");
+        }
+    });
+
+    router.put('/v2/test', function (req, res, next) {
+        if (req.query["api-version"] === '2.0.0' && req.query["id"] == 2) {
+            optionalCoverage['MultiapiCustomBaseUrlApiVersionTwo']++;
+            res.status(200).type('json').end();
+        } else {
+            utils.send400(res, next, "Api version needs to be 2.0.0 and you should pass in 2 for 'id'");
+        }
+    });
+}
+
+
+multiapiCustomBaseUrl.prototype.router = router;
+
+module.exports = multiapiCustomBaseUrl;
