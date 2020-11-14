74b866dad932d5c22d7f874ef762f5a5b4d69f66
iscai-msft <iscai@microsoft.com>
2020-11-13 19:31:35 -0500

    add tests for metadata and token

--- legacy/routes/pagingSpecial.js
+++ legacy/routes/pagingSpecial.js
@@ -5,13 +5,16 @@ var utils = require('../util/utils');
 
 var pagingSpecial = function(optionalCoverage) {
     optionalCoverage['PagingSpecialNextLinkInResponseHeaders'] = 0;
+    optionalCoverage['PagingSpecialContinuationToken'] = 0;
     optionalCoverage["PagingSpecialContinuationTokenInResponseHeaders"] = 0;
+    optionalCoverage["PagingSpecialContinuationTokenWithMetadata"] = 0;
+
 
     router.get('/nextLinkInResponseHeaders', function(req, res, next) {
         var headers = {
             'x-ms-nextLink': '/pagingSpecial/nextLinkInResponseHeaders/page/2',
         };
-        res.set(headers).status(200).json({ "value" : [ {"properties":{"id": 1, "name": "Product" }}] });
+        res.set(headers).status(200).json({ "value" : [ {"properties":{"name": "Product" }}] });
     });
 
 
@@ -20,35 +23,68 @@ var pagingSpecial = function(optionalCoverage) {
             var headers = {
                 'x-ms-nextLink': '/pagingSpecial/nextLinkInResponseHeaders/page/'  + (++req.params.pagenumber),
             };
-            res.set(headers).status(200).json({"value": [ {"properties":{"id" : parseInt(req.params.pagenumber), "name": "product"}} ]});
+            res.set(headers).status(200).json({"value": [ {"properties":{"name": "product"}} ]});
         } else {
-            optionalCoverage['PagingSpecialNextLinkInResponseHeaders'] = 0;
-            res.status(200).json({"value": [ {"properties":{"id" : parseInt(req.params.pagenumber), "name": "product"}} ]});
+            optionalCoverage['PagingSpecialNextLinkInResponseHeaders']++;
+            res.status(200).json({"value": [ {"properties":{"name": "product"}} ]});
         }
     });
 
-    router.get('/continuationTokenInResponseHeaders', function(req, res, next) {
-        
-        if (req.headers['continuationtoken']) {
-            contToken = req.headers['continuationtoken']
+    router.get('/continuationToken', function(req, res, next) {
+
+        if (req.headers["x-ms-token"]) {
+            contToken = req.headers["x-ms-token"]
             if (contToken < 10) {
-                var headers = {
-                    'continuationtoken': ++contToken,
-                };
-                res.set(headers).status(200).json({ "value" : [ {"properties":{"id": contToken, "name": "Product" }}] });
+                res.status(200).json({ "value": [ {"properties":{"name": "product"}} ], "token": ++contToken});
             } else {
-                res.status(200).json({"value": [ {"properties":{"id" : contToken, "name": "product"}} ]});
+                res.status(200).json({"value": [ {"properties":{"name": "product"}} ]});
             }
 
         } else {
-            console.log('first')
-            var headers = {
-                'continuationtoken': 2,
-            };
-            res.set(headers).status(200).json({ "value" : [ {"properties":{"id": 1, "name": "Product" }}] });
+            optionalCoverage['PagingSpecialContinuationToken']++;
+            res.status(200).json({ "value": [ {"properties":{"name": "product"}} ], "token": 2});
         }
 
     });
+
+    router.get('/continuationTokenInResponseHeaders', function(req, res, next) {
+
+        if (req.headers["x-ms-token"]) {
+            contToken = req.headers["x-ms-token"]
+            if (contToken < 10) {
+                var headers = {
+                    "x-ms-token": ++contToken,
+                };
+                res.set(headers).status(200).json({ "value" : [ {"properties":{"name": "Product" }}] });
+            } else {
+                optionalCoverage["PagingSpecialContinuationTokenInResponseHeaders"]++;
+                res.status(200).json({"value": [ {"properties":{"name": "product"}} ]});
+            }
+
+        } else {
+            var headers = {
+                "x-ms-token": 2,
+            };
+            res.set(headers).status(200).json({ "value" : [ {"properties":{"name": "Product" }}] });
+        }
+
+    });
+
+    router.get('/tokenWithMetadata', function(req, res, next) {
+        if (req.headers["x-ms-token"]) {
+            contToken = req.headers["x-ms-token"]
+            if (contToken < 10) {
+                res.status(200).json({ "value": [ {"properties":{"name": "product"}} ], "token": ++contToken + ";10"});
+            } else {
+                optionalCoverage['PagingSpecialContinuationTokenWithMetadata']++;
+                res.status(200).json({"value": [ {"properties":{"name": "product"}} ]});
+            }
+
+        } else {
+            res.status(200).json({ "value": [ {"properties":{"name": "product"}} ], "token": "2;10"});
+        }
+    });
+
 };
 
 pagingSpecial.prototype.router = router;
