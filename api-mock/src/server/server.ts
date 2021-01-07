f9b0217ebf1cae6a620f8d538eab191c85de6110
Timothee Guerin <tiguerin@microsoft.com>
2021-01-07 15:38:12 -0800

    Add basic server

--- api-mock/src/server/server.ts
+++ api-mock/src/server/server.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import { MockRouteDefinition } from "../models";
 
 export class MockApiServer {
   private app: express.Application;
 
   constructor() {
     this.app = express();
+  }
+
+  public add(route: MockRouteDefinition): void {
+    this.app.route(route.request.url)[route.request.method]((_, res) => {
+      res.status(route.response.status).contentType(route.response.body.contentType).send(route.response.body.content);
+    });
+  }
+
+  public addMultiple(routes: MockRouteDefinition[]): void {
+    for (const route of routes) {
+      this.add(route);
+    }
   }
 }
