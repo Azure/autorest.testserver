720b8b18476d7f881ca61ccbbac273904fc3edea
Timothee Guerin <tiguerin@microsoft.com>
2021-01-13 12:59:51 -0800

    WIp

--- v2/src/routes/coverage.ts
+++ v2/src/routes/coverage.ts
@@ -4,6 +4,11 @@ import { coverageService } from "../services";
 const router = Router();
 export const coverageRouter = router;
 
+router.get("/report/clear", (_req, res) => {
+  coverageService.reset();
+  return res.status(200).send().end();
+});
+
 router.get("/report/:category?", (req, res) => {
   const category = req.params.category?.toString();
   return res.json(coverageService.getAllForCategory(category)).end();
