3b8c1bde85fe2b8edc37eb73f5bc251e5441c318
iscai-msft <iscai@microsoft.com>
2021-01-22 12:24:54 -0500

    add test for error deserialization

---
+++ src/test-routes/incorrect-error-response.ts
@@ -0,0 +1,13 @@
+
+import { app, json } from "../api";
+app.category("vanilla", () => {
+    app.get("/incorrectError", "verifyIncorrectErrorParsing", (req) => {
+        return {
+            status: 444,
+            body: {
+                contentType: "text/html",
+                rawContent: "foobar"
+            }
+          };
+    });
+});
