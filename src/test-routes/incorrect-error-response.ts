924a5e908dd8609d342e96968ae1580cfb9fe8b9
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2021-01-25 11:34:55 -0500

    add test for error deserialization (#257)

---
+++ src/test-routes/incorrect-error-response.ts
@@ -0,0 +1,12 @@
+import { app, json } from "../api";
+app.category("vanilla", () => {
+  app.get("/incorrectError", "verifyIncorrectErrorParsing", (req) => {
+    return {
+      status: 444,
+      body: {
+        contentType: "text/html",
+        rawContent: "foobar",
+      },
+    };
+  });
+});
