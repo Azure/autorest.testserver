937e6e43dedea87496fb72d0756030c4c86949ca
Laurent Mazuel <laurent.mazuel@gmail.com>
2021-07-02 16:10:23 -0700

    Update llc.ts

--- src/test-routes/llc.ts
+++ src/test-routes/llc.ts
@@ -1,7 +1,7 @@
 import { app, json } from "../api";
 
 app.category("vanilla", () => {
-  app.get("/llc/parameters", "LLCRequiredTooptional", (req) => {
+  app.get("/llc/parameters", "LLCRequiredToOptional", (req) => {
     return {
       status: 200,
       body: json({ message: `An object was successfully returned` })
