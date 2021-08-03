340d72cefb4143cd105997f6615210f11bf54452
iscai-msft <iscai@microsoft.com>
2021-08-03 13:43:47 -0400

    prettier test

--- src/test-routes/media-types.ts
+++ src/test-routes/media-types.ts
@@ -4,7 +4,7 @@ app.category("vanilla", () => {
   app.post("/mediatypes/analyzeNoAccept", "MediaTypesAnalyzeBodyNoAcceptHeader", (req) => {
     const contentType = req.headers["content-type"];
     const body = req.body;
-    if (contentType === 'application/json' && 'source' in body) {
+    if (contentType === "application/json" && "source" in body) {
       return {
         status: 202,
       };
