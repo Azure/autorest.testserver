b86d58b3f75a6124d21565dc30a3bbb59cb9ae09
Joel Hendrix <jhendrix@microsoft.com>
2021-09-29 10:20:11 -0700

    Fix up binary swagger and add test routes (#317)
    * Fix up binary swagger and add test routes
    
    * bump version
    
    * fix linter issues

---
+++ src/test-routes/binary-files.ts
@@ -0,0 +1,14 @@
+import { app, json } from "../api";
+
+app.category("vanilla", () => {
+  app.post("/binary/file", "BodyBinaryFile", (req) => {
+    req.expect.containsHeader("content-type", "application/json");
+    req.expect.bodyEquals({ more: "cowbell" });
+    return { status: 200 };
+  });
+  app.put("/binary/octet", "BodyBinaryOctet", (req) => {
+    req.expect.containsHeader("content-type", "application/octet-stream");
+    req.expect.bodyNotEmpty();
+    return { status: 200 };
+  });
+});
