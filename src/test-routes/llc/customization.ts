06d0de2f619e23fe0aeab48a97145955102855d1
iscai-msft <iscai@microsoft.com>
2021-10-07 17:40:06 -0400

    add models to swagger

--- src/test-routes/llc/customization.ts
+++ src/test-routes/llc/customization.ts
@@ -1,18 +1,33 @@
 import { app, json } from "../../api";
 
 app.category("llc", () => {
-  app.get("/customization/getModel/raw", "LLCGetRawModel", (req) => {
+  app.get("/customization/model/raw", "LLCGetRawModel", (req) => {
     return {
       status: 200,
       body: json({ received: "raw" }),
     };
   });
-  app.get("/customization/getModel/model", "LLCGetHandwrittenModel", (req) => {
+  app.get("/customization/model/model", "LLCGetHandwrittenModel", (req) => {
     return {
       status: 200,
       body: json({ received: "model" }),
     };
   });
+  app.post("/customization/model/raw", "LLCPostRawModel", (req) => {
+    req.expect.bodyEquals({ hello: `world!` });
+    return {
+      status: 200,
+      body: json({ received: "raw" }),
+    };
+  });
+  app.post("/customization/model/model", "LLCPostHandwrittenModel", (req) => {
+    req.expect.bodyEquals({ hello: `world!` });
+    return {
+      status: 200,
+      body: json({ received: "model" }),
+    };
+  });
+
   app.get("/customization/paging/raw/", "LLCGetRawPages", (req) => {
     return {
       status: 200,
@@ -29,6 +44,19 @@ app.category("llc", () => {
     return {
       status: 200,
       body: json({ values: [{ "received": req.params.mode?.toString() }] })
+    };
+  });
+
+  app.put("/customization/lro/raw", "LLCRawLRO", (req) => {
+    return {
+      status: 200,
+      body: json({ provisioningState: "Succeeded", received: "raw" }),
+    };
+  });
+  app.put("/customization/lro/model", "LLCHandwrittenModelLRO", (req) => {
+    return {
+      status: 200,
+      body: json({ provisioningState: "Succeeded", received: "model" }),
     };
   });
 });
