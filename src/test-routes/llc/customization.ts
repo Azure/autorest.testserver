6ee0081d2b093d72c0936e7b0c9748bfd51bda69
iscai-msft <iscai@microsoft.com>
2021-10-07 14:00:58 -0400

    add paging customization tests

--- src/test-routes/llc/customization.ts
+++ src/test-routes/llc/customization.ts
@@ -13,4 +13,22 @@ app.category("llc", () => {
       body: json({ received: "model" }),
     };
   });
+  app.get("/customization/paging/raw/", "LLCGetRawPages", (req) => {
+    return {
+      status: 200,
+      body: json({ values: [{ "received": "raw" }], "nextLink": req.baseUrl + "/customization/paging/raw/2" })
+    };
+  });
+  app.get("/customization/paging/model/", "LLCGetHandwrittenModelPages", (req) => {
+    return {
+      status: 200,
+      body: json({ values: [{ "received": "model" }], "nextLink": req.baseUrl + "/customization/paging/model/2" })
+    };
+  });
+  app.get("/customization/paging/:mode/2", "", (req) => {
+    return {
+      status: 200,
+      body: json({ values: [{ "received": req.params.mode?.toString() }] })
+    };
+  });
 });
