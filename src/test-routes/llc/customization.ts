820a15f3c976f4fea58cf1827c73ef329d7e70c5
iscai-msft <43154838+iscai-msft@users.noreply.github.com>
2021-10-14 17:11:05 -0700

    add initial LLC tests (#320)
    * add llc customization swagger and get model test
    
    * add paging customization tests
    
    * add models to swagger
    
    * bump package version
    
    * run prettier
    
    * rename handwritten to grown up models
    
    * remove LLC prefix from tests
    
    * move coverage checks to second page

---
+++ src/test-routes/llc/customization.ts
@@ -0,0 +1,65 @@
+import { app, json } from "../../api";
+
+app.category("llc", () => {
+  app.get("/customization/model/raw", "GetRawModel", (req) => {
+    return {
+      status: 200,
+      body: json({ received: "raw" }),
+    };
+  });
+  app.get("/customization/model/model", "GetHandwrittenModel", (req) => {
+    return {
+      status: 200,
+      body: json({ received: "model" }),
+    };
+  });
+  app.post("/customization/model/raw", "PostRawModel", (req) => {
+    req.expect.bodyEquals({ hello: `world!` });
+    return {
+      status: 200,
+      body: json({ received: "raw" }),
+    };
+  });
+  app.post("/customization/model/model", "PostHandwrittenModel", (req) => {
+    req.expect.bodyEquals({ hello: `world!` });
+    return {
+      status: 200,
+      body: json({ received: "model" }),
+    };
+  });
+
+  app.get("/customization/paging/:mode/", "", (req) => {
+    return {
+      status: 200,
+      body: json({
+        values: [{ received: "model" }],
+        nextLink: req.baseUrl + "/customization/paging/" + req.params.mode + "/2",
+      }),
+    };
+  });
+  app.get("/customization/paging/raw/2", "GetRawPages", (req) => {
+    return {
+      status: 200,
+      body: json({ values: [{ received: req.params.mode?.toString() }] }),
+    };
+  });
+  app.get("/customization/paging/model/2", "GetHandwrittenModelPages", (req) => {
+    return {
+      status: 200,
+      body: json({ values: [{ received: req.params.mode?.toString() }] }),
+    };
+  });
+
+  app.put("/customization/lro/raw", "RawLRO", (req) => {
+    return {
+      status: 200,
+      body: json({ provisioningState: "Succeeded", received: "raw" }),
+    };
+  });
+  app.put("/customization/lro/model", "HandwrittenModelLRO", (req) => {
+    return {
+      status: 200,
+      body: json({ provisioningState: "Succeeded", received: "model" }),
+    };
+  });
+});
