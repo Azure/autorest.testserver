9282783eae8b6c69d165655a2f764dae5e0ba5fd
Laurent Mazuel <laurent.mazuel@gmail.com>
2021-10-21 19:49:57 -0700

    Rework service driven evolution (#321)
    * Rework service driven evolution
    
    * Not required
    
    * fix any definition for body parameters
    
    * make post input a model
    
    * prettier
    
    * Version
    
    * prettier
    
    Co-authored-by: iscai-msft <iscai@microsoft.com>
    Co-authored-by: Timothee Guerin <tiguerin@microsoft.com>

--- src/test-routes/llc/update.ts
+++ src/test-routes/llc/update.ts
@@ -1,7 +1,39 @@
-import { app, json } from "../../api";
+import { app, json, ValidationError } from "../../api";
+import { coverageService } from "../../services";
 
-app.category("optional", () => {
-  app.get("/llc/parameters", "LLCRequiredToOptional", (req) => {
+app.category("llc", () => {
+  app.get("/servicedriven/parameters", "LLCAddOptionalInput", (req) => {
+    return {
+      status: 200,
+      body: json({ message: `An object was successfully returned` }),
+    };
+  });
+
+  coverageService.register("llc", "LLCNewBodyType.JSON");
+  coverageService.register("llc", "LLCNewBodyType.JPEG");
+
+  app.post("/servicedriven/parameters", "LLCNewBodyType", (req) => {
+    switch (req.headers["content-type"]) {
+      case "image/jpeg":
+        // req.expect.rawBodyEquals("binary");
+        coverageService.track("llc", "LLCNewBodyType.JPEG");
+        return { status: 200 };
+      case "application/json":
+        req.expect.bodyEquals({ url: "http://example.org/myimage.jpeg" });
+        coverageService.track("llc", "LLCNewBodyType.JSON");
+        return { status: 200 };
+      default:
+        throw new ValidationError("Should be image/jpeg or application/json", {}, req.headers["content-type"]);
+    }
+  });
+
+  app.delete("/servicedriven/parameters", "LLCAddNewOperation", (req) => {
+    return {
+      status: 204,
+    };
+  });
+
+  app.get("/servicedriven/newpath", "LLCAddNewPath", (req) => {
     return {
       status: 200,
       body: json({ message: `An object was successfully returned` }),
