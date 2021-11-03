48ebcf3a41972d43d1378e9b3acee2c29a0d1b96
iscai-msft <iscai@microsoft.com>
2021-11-03 13:35:57 -0700

    check binary body in media types testserver

--- src/test-routes/media-types.ts
+++ src/test-routes/media-types.ts
@@ -21,7 +21,7 @@ app.category("vanilla", () => {
   app.post("/mediatypes/binaryBodyTwoContentTypes", "MediaTypesTwoContentTypes", (req) => {
     const contentType = req.headers["content-type"];
     if (contentType === "application/json") {
-      req.expect.bodyNotEmpty();
+      req.expect.bodyEquals({ "hello": "world" });
       return {
         status: 200,
       };
