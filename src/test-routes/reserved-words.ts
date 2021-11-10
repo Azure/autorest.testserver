da9c9652aaf13dc886dfbbf803b9808dc6cf45a7
iscai-msft <iscai@microsoft.com>
2021-11-10 15:40:44 -0800

    add tests for reserved body names

--- src/test-routes/reserved-words.ts
+++ src/test-routes/reserved-words.ts
@@ -7,4 +7,20 @@ app.category("vanilla", () => {
     }
     return { status: 400 };
   });
+
+  app.put("/reservedWords/operation/content", "reservedWordsBodyNamedContent", (req) => {
+    req.expect.bodyEquals(Buffer.from("hello, world"));
+    return { status: 200 };
+  });
+  app.put("/reservedWords/operation/json", "reservedWordsBodyNamedJson", (req) => {
+    req.expect.bodyEquals({ hello: "world" });
+    return { status: 200 };
+  });
+  app.put("/reservedWords/operation/data", "reservedWordsBodyNamedData", (req) => {
+    req.expect.bodyEquals({ data: "hello", world: "world" });
+    return { status: 200 };
+  });
+  app.put("/reservedWords/operation/files", "reservedWordsBodyNamedFiles", (req) => {
+    return { status: 200 };
+  });
 });
