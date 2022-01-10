aa4692a18478020b96af072a4af06c74545814d0
iscai-msft <iscai@microsoft.com>
2022-01-10 15:38:25 -0500

    switch to contains header

--- src/test-routes/merge-patch-json.ts
+++ src/test-routes/merge-patch-json.ts
@@ -1,15 +1,12 @@
+import { request } from "express";
 import { app, json } from "../api";
 
 app.category("vanilla", () => {
   app.patch("/mergePatchJson/single", "PassMergePatchObject", (req) => {
-    if (req.headers["content-type"] === "application/merge-patch+json" && req.headers["content-length"] === "14") {
-      return {
-        status: 200,
-      };
-    } else {
-      return {
-        status: 400,
-      };
-    }
+    req.expect.containsHeader("content-type", "application/merge-patch+json");
+    req.expect.containsHeader("content-length", "14");
+    return {
+      status: 200,
+    };
   });
 });
