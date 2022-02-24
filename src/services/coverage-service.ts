08cba1c8ce0b82387d593a67bc21708d09fdaa21
Timothee Guerin <tiguerin@microsoft.com>
2022-02-24 14:34:47 -0800

    Fix: Legacy tests resetting existing coverage (#351)

--- src/services/coverage-service.ts
+++ src/services/coverage-service.ts
@@ -53,7 +53,9 @@ export class CoverageService {
       map[name] = 0;
     }
 
-    map[name] = value;
+    if (value > map[name]) {
+      map[name] = value;
+    }
     this.legacySaveCoverage(category);
   }
 
