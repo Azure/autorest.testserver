72e805767bc1de4259cd6d525e081aa783446f32
Timothee Guerin <tiguerin@microsoft.com>
2022-02-25 00:24:19 -0800

    Fix

--- src/test-routes/paths.ts
+++ src/test-routes/paths.ts
@@ -1,5 +1,4 @@
-import { escape } from "querystring";
-import { app, json, ValidationError } from "../api";
+import { app, ValidationError } from "../api";
 const Constants = {
   DEFAULT_SERVER_PORT: "3000",
 
@@ -20,38 +19,42 @@ const scenarios = {
   int: {
     name: "Int",
     scenarios: {
-      "1000000": "Positive",
-      "-1000000": "Negative",
-      "1460505600": "UnixTime",
+      "1000000": { name: "Positive", expectedValue: 1000000 },
+      "-1000000": { name: "Negative", expectedValue: -1000000 },
+      "1460505600": { name: "UnixTime", expectedValue: 1460505600 },
     },
   },
   long: {
     name: "Long",
     scenarios: {
-      "10000000000": "Positive",
-      "-10000000000": "Negative",
+      "10000000000": { name: "Positive", expectedValue: 10000000000 },
+      "-10000000000": { name: "Negative", expectedValue: -10000000000 },
     },
   },
   float: {
     name: "Float",
     scenarios: {
-      "1.034E+20": "Positive",
-      "-1.034E-20": "Negative",
+      "1.034E\\+20": { name: "Positive", expectedValue: 1.034e20, skipEscape: true },
+      "-1.034E-20": { name: "Negative", expectedValue: -1.034e-20 },
     },
   },
   double: {
     name: "Double",
     scenarios: {
-      "9999999.999": "Positive",
-      "-9999999.999": "Negative",
+      "9999999.999": { name: "Positive", expectedValue: 9999999.999 },
+      "-9999999.999": { name: "Negative", expectedValue: -9999999.999 },
     },
   },
   string: {
     name: "String",
     scenarios: {
-      "unicode": "Unicode",
+      "unicode": { name: "Unicode", expectedValue: Constants.MULTIBYTE_BUFFER },
       "begin!*'();:@ &=+$,/?#[]end": "UrlEncoded",
-      "begin!*'();:@&=+$,end": "UrlNonEncoded",
+      "begin!*'\\(\\)\\;\\:\\@\\&\\=\\+\\$\\,end": {
+        name: "UrlNonEncoded",
+        expectedValue: "begin!*'();:@&=+$,end",
+        skipEscape: true,
+      },
       "null": "Null",
       "bG9yZW0": "Base64Url",
     },
@@ -59,7 +62,7 @@ const scenarios = {
   byte: {
     name: "Byte",
     scenarios: {
-      multibyte: "MultiByte",
+      multibyte: { name: "MultiByte", expectedValue: Buffer.from(Constants.MULTIBYTE_BUFFER).toString("base64") },
     },
   },
   date: {
@@ -71,7 +74,7 @@ const scenarios = {
   datetime: {
     name: "DateTime",
     scenarios: {
-      "2012-01-01T01:01:01Z": "Valid",
+      "2012-01-01T01:01:01Z": { name: "Valid", expectedValue: new Date("2012-01-01T01:01:01Z").toISOString() },
     },
   },
   enum: {
@@ -88,14 +91,22 @@ const scenarios = {
   },
 } as const;
 
-function validateArrayPath(arrayValue: string, separator: string): boolean {
-  return arrayValue === "ArrayPath1" + separator + "begin!*'();:@ &=+$,/?#[]end" + separator + separator;
+type Scenarios = typeof scenarios;
+
+interface ScenarioConfig {
+  name: string;
+  expectedValue: unknown;
+  skipEscape?: boolean;
 }
 
-type Entries<T> = {
-  [K in keyof T]: [K, T[K]];
-}[keyof T][];
-
+function encodeSegement(value: string): string {
+  return encodeURIComponent(value)
+    .replaceAll("!", "%21")
+    .replaceAll("*", "%2A")
+    .replaceAll("'", "%27")
+    .replaceAll("(", "%28")
+    .replaceAll(")", "%29");
+}
 app.category("vanilla", () => {
   for (const [type, value] of Object.entries(scenarios)) {
     app.get(`/paths/${type}/empty`, `UrlPaths${value.name}Empty`, (req) => {
@@ -103,13 +114,15 @@ app.category("vanilla", () => {
         status: 200,
       };
     });
-    for (const [scenario, scenarioName] of Object.entries(value.scenarios)) {
-      const coverageName = `UrlPaths${value.name}${scenarioName}`;
+    for (const [scenario, scenarioConfig] of Object.entries<string | ScenarioConfig>(value.scenarios)) {
+      const coverageName = `UrlPaths${value.name}${getScenarioName(scenarioConfig)}`;
       // Convert the string value to a javascript primtive if possible.
+      const scenarioSegment =
+        typeof scenarioConfig === "object" && scenarioConfig.skipEscape ? scenario : encodeSegement(scenario);
 
-      app.get(`/paths/${type}/${escape(scenario)}/:wireParameter`, coverageName, (req) => {
-        const wireParameter = req.params.wireParameter;
-        const expectedValue = getExpectedValue(type as never, scenarioName);
+      app.get(`/paths/${type}/${scenarioSegment}/:wireParameter`, coverageName, (req) => {
+        const wireParameter = deserializeValue(type as never, req.params.wireParameter);
+        const expectedValue = getScenarioExpectedValue(scenario, scenarioConfig);
 
         if (wireParameter !== expectedValue) {
           throw new ValidationError("wireParameter path does not match expected value", expectedValue, wireParameter);
@@ -121,17 +134,22 @@ app.category("vanilla", () => {
   }
 });
 
-function getExpectedValue<T extends keyof typeof scenarios, S extends keyof typeof scenarios[T]["scenarios"]>(
-  type: T,
-  scenario: S,
-) {
-  const defaultValue = JSON.parse(`"${scenario}"`);
+function deserializeValue<T extends keyof Scenarios>(type: T, value: string) {
   switch (type) {
-    case "string":
-      return scenario === "unicode" ? Constants.MULTIBYTE_BUFFER : defaultValue;
-    case "byte":
-      return Buffer.from(Constants.MULTIBYTE_BUFFER).toString("base64");
+    case "int":
+    case "long":
+    case "float":
+    case "double":
+      return JSON.parse(decodeURIComponent(value));
     default:
-      return defaultValue;
+      return value;
   }
+}
+
+function getScenarioName(scenarioConfig: string | ScenarioConfig) {
+  return typeof scenarioConfig === "string" ? scenarioConfig : scenarioConfig.name;
+}
+
+function getScenarioExpectedValue(scenarioPath: string, scenarioConfig: string | ScenarioConfig) {
+  return typeof scenarioConfig === "string" ? scenarioPath : scenarioConfig.expectedValue;
 }
