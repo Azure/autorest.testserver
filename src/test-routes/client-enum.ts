0d3bf89337284f7bc1229599d98e45bd9f995686
iscai-msft <iscai@microsoft.com>
2022-11-04 14:35:02 -0700

    bump version and fix client enum test

--- src/test-routes/client-enum.ts
+++ src/test-routes/client-enum.ts
@@ -1,7 +1,7 @@
 import { app } from "../api";
 
 app.category("vanilla", () => {
-  app.head("/clientEnum/head", "getBoolTrue", (req) => {
+  app.head("/clientEnum/head", "ClientWithEnumParam", (req) => {
     req.expect.containsHeader("x-ms-enum", "single");
     return {
       status: 200,
