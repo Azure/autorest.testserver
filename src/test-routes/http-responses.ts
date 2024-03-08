Azure/autorest.testserver

commit df06807fec15a4feb19833aeaa7df17def17f82d

Fix issue with invalid endpoints causing missing coverage (#364)

src/test-routes/http-responses.ts
@@ -32,15 +32,15 @@ app.category("vanilla", () => {
     "/success",
     "Success",
     {
-      200: ["head", "get", "options", "put", "post", "patch", "delete"],
+      200: ["head", "get", "put", "post", "patch", "delete"],
       201: ["put", "post"],
       202: ["put", "post", "patch", "delete"],
       204: ["head", "put", "post", "patch", "delete"],
       404: ["head"],
     },
     ({ method, code }) => {
-      const body = method === "get" || method === "options" ? json(true) : undefined;
-      return { status: Number(code), body };
+      const body = method === "get" ? json(true) : undefined;
+      return { status: Number(code), body, testSuccessful: true };
     },
   );
 
@@ -61,7 +61,7 @@ app.category("vanilla", () => {
       301: ["head", "put", "get"],
       302: ["head", "get", "patch"],
       303: ["post"],
-      307: ["head", "get", "options", "put", "post", "patch", "delete"],
+      307: ["head", "get", "put", "post", "patch", "delete"],
     },
     ({ code, method }) => {
       let location;
@@ -90,10 +90,10 @@ app.category("vanilla", () => {
     "/failure/client",
     "ClientFailure",
     {
-      400: ["head", "get", "options", "put", "post", "patch", "delete"],
+      400: ["head", "get", "put", "post", "patch", "delete"],
       401: ["head"],
       402: ["get"],
-      403: ["get", "options"],
+      403: ["get"],
       404: ["put"],
       405: ["patch"],
       406: ["post"],
@@ -101,7 +101,7 @@ app.category("vanilla", () => {
       409: ["put"],
       410: ["head"],
       411: ["get"],
-      412: ["get", "options"],
+      412: ["get"],
       413: ["put"],
       414: ["patch"],
       415: ["post"],
@@ -122,8 +122,6 @@ app.category("vanilla", () => {
     "ServerFailure",
     {
       501: ["head", "get"],
-      502: ["options", "put"],
-      504: ["patch"],
       505: ["post", "delete"],
     },
     ({ code, method }) => {
@@ -139,7 +137,7 @@ app.category("vanilla", () => {
     "Retry",
     {
       408: ["head"],
-      502: ["options", "get"],
+      502: ["get"],
       500: ["put", "patch"],
       503: ["post", "delete"],
       504: ["put", "patch"],

src/api/mock-api-router.ts
@@ -5,7 +5,7 @@ import { RequestExt } from "../server";
 import { coverageService } from "../services";
 import { MockRequestHandler, processRequest } from "./request-processor";
 
-export type HttpMethod = "get" | "post" | "put" | "patch" | "delete" | "head" | "options";
+export type HttpMethod = "get" | "post" | "put" | "patch" | "delete" | "head";
 
 export type Category = "vanilla" | "azure" | "dpg" | "optional";
 
@@ -78,16 +78,6 @@ export class MockApiRouter {
    */
   public delete(uri: string, name: string | undefined, func: MockRequestHandler): void {
     this.request("delete", uri, name, func);
-  }
-
-  /**
-   * Register a OPTIONS request for the provided uri.
-   * @param uri URI to match.
-   * @param name Name of the scenario(For coverage).
-   * @param func Request handler.
-   */
-  public options(uri: string, name: string | undefined, func: MockRequestHandler): void {
-    this.request("options", uri, name, func);
   }
 
   /**

src/cli/commands/validate-spec-coverage-command.ts
@@ -82,7 +82,10 @@ const validateRouteDefined = (path: SpecPath, registeredPaths: Layer[]): HttpMet
     }
   }
 
-  return path.methods.filter((x) => !methodFound[x]).filter((x) => !x.startsWith("x-"));
+  return path.methods
+    .filter((x): x is HttpMethod => x !== "options")
+    .filter((x) => !methodFound[x])
+    .filter((x) => !x.startsWith("x-"));
 };
 
 const loadRegisteredRoutes = async (): Promise<Layer[]> => {

src/services/spec-coverage-service.ts
@@ -10,7 +10,7 @@ export interface SpecPath {
   /**
    * List of HTTP methods to implement.
    */
-  methods: HttpMethod[];
+  methods: (HttpMethod | "options")[];
 }
 
 export const getPathsFromSpecs = async (specPaths: string[]): Promise<SpecPath[]> => {

src/test-routes/array.ts
@@ -303,15 +303,6 @@ app.category("vanilla", () => {
       body: json(["YSBzdHJpbmcgdGhhdCBnZXRzIGVuY29kZWQgd2l0aCBiYXNlNjR1cmw", "dGVzdCBzdHJpbmc", "TG9yZW0gaXBzdW0"]),
     };
   });
-
-  app.put("/array/prim/base64url/valid", "putArrayBase64Url", (req) => {
-    req.expect.bodyEquals([
-      "YSBzdHJpbmcgdGhhdCBnZXRzIGVuY29kZWQgd2l0aCBiYXNlNjR1cmw",
-      "dGVzdCBzdHJpbmc",
-      "TG9yZW0gaXBzdW0",
-    ]);
-    return { status: 200 };
-  });
   //#endregion
 
   //#region complex

src/test-routes/paths.ts
@@ -49,19 +49,22 @@ const scenarios = {
   string: {
     name: "String",
     scenarios: {
+      "empty": "Empty",
       "unicode": { name: "Unicode", expectedValue: Constants.MULTIBYTE_BUFFER },
       "begin!*'();:@ &=+$,/?#[]end": "UrlEncoded",
       "begin!*'();:@&=+$,end": {
         name: "UrlNonEncoded",
         expectedValue: "begin!*'();:@&=+$,end",
       },
-      "null": "Null",
+      // This scenario shouldn't really be possible. The sdk should never call the endpoint.
+      // "null": "Null",
       "bG9yZW0": "Base64Url",
     },
   },
   byte: {
     name: "Byte",
     scenarios: {
+      empty: "Empty",
       multibyte: { name: "MultiByte", expectedValue: Buffer.from(Constants.MULTIBYTE_BUFFER).toString("base64") },
     },
   },
@@ -103,21 +106,29 @@ interface ScenarioConfig {
 
 app.category("vanilla", () => {
   for (const [type, value] of Object.entries(scenarios)) {
-    app.get(`/paths/${type}/empty`, `UrlPaths${value.name}Empty`, (req) => {
-      return {
-        status: 200,
-      };
-    });
-
     for (const scenarioConfig of Object.values<string | ScenarioConfig>(value.scenarios)) {
       const coverageName = `UrlPaths${value.name}${getScenarioName(scenarioConfig)}`;
       coverageService.register("vanilla", coverageName);
     }
 
-    app.get(`/paths/${type}/:scenarioName/:wireParameter`, undefined, (req) => {
-      const wireParameter = deserializeValue(type as never, req.params.wireParameter);
+    app.get(`/paths/${type}/:scenarioName/:wireParameter?`, undefined, (req) => {
       const scenarioName: string = decodeURIComponent(req.params.scenarioName);
       const scenarioConfig: ScenarioConfig = value.scenarios[scenarioName as keyof typeof value.scenarios];
+      const coverageName = `UrlPaths${value.name}${getScenarioName(scenarioConfig)}`;
+
+      if (scenarioName === "empty") {
+        if (req.params.wireParameter) {
+          throw new ValidationError("wireParameter should be empty", undefined, req.params.wireParameter);
+        }
+        coverageService.track("vanilla", coverageName);
+
+        return {
+          status: 200,
+        };
+      }
+
+      const wireParameter = deserializeValue(type as never, req.params.wireParameter);
+
       if (scenarioConfig === undefined) {
         return { status: 404, body: json({ message: `Scenario "${scenarioName}" not found for type ${type}` }) };
       }
@@ -127,7 +138,7 @@ app.category("vanilla", () => {
         throw new ValidationError("wireParameter path does not match expected value", expectedValue, wireParameter);
       }
 
-      coverageService.track("vanilla", `UrlPaths${value.name}${getScenarioName(scenarioConfig)}`);
+      coverageService.track("vanilla", coverageName);
       return { status: 200 };
     });
   }

commit 504793629cff35a0676a566768f3be24cfd12b4a

Add options method back to some tests
Fix regression introduced in df06807.

src/test-routes/http-responses.ts
@@ -90,10 +90,10 @@ app.category("vanilla", () => {
     "/failure/client",
     "ClientFailure",
     {
-      400: ["head", "get", "put", "post", "patch", "delete"],
+      400: ["head", "get", "options", "put", "post", "patch", "delete"],
       401: ["head"],
       402: ["get"],
-      403: ["get"],
+      403: ["get", "options"],
       404: ["put"],
       405: ["patch"],
       406: ["post"],
@@ -101,7 +101,7 @@ app.category("vanilla", () => {
       409: ["put"],
       410: ["head"],
       411: ["get"],
-      412: ["get"],
+      412: ["get", "options"],
       413: ["put"],
       414: ["patch"],
       415: ["post"],